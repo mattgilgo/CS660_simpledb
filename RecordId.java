82a7fe889452151f4295154d20fa4817dced47f3
shilpen12 <70973088+shilpen12@users.noreply.github.com>
2021-10-01 11:13:40 -0500

    Add files via upload

---
+++ RecordId.java
@@ -0,0 +1,89 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A RecordId is a reference to a specific tuple on a specific page of a
+ * specific table.
+ */
+public class RecordId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private PageId pid;
+    private int tupleno;
+    /**
+     * Creates a new RecordId referring to the specified PageId and tuple
+     * number.
+     * 
+     * @param pid
+     *            the pageid of the page on which the tuple resides
+     * @param tupleno
+     *            the tuple number within the page.
+     */
+    public RecordId(PageId pid, int tupleno) {
+        // some code goes here
+        this.pid = pid;
+        this.tupleno = tupleno;
+    }
+
+    /**
+     * @return the tuple number this RecordId references.
+     */
+    public int tupleno() {
+        // some code goes here
+        return this.tupleno;
+    }
+
+    /**
+     * @return the page id this RecordId references.
+     */
+    public PageId getPageId() {
+        // some code goes here
+        return this.pid;
+    }
+
+    /**
+     * Two RecordId objects are considered equal if they represent the same
+     * tuple.
+     * 
+     * @return True if this and o represent the same tuple
+     */
+    @Override
+    public boolean equals(Object o) {
+        // some code goes here
+        //throw new UnsupportedOperationException("implement this");
+            // some code goes here
+        RecordId rid;
+        if (this == o) return true;
+        if (o == null) {
+            return false;
+        }
+        try{
+            rid = (RecordId) o;
+        } catch (ClassCastException cce){
+            return false;
+        }
+        if (this.pid != rid.getPageId()) {
+            return false;
+        }
+        return true;
+        }
+
+    /**
+     * You should implement the hashCode() so that two equal RecordId instances
+     * (with respect to equals()) have the same hashCode().
+     * 
+     * @return An int that is the same for equal RecordId objects.
+     */
+    @Override
+    public int hashCode() {
+        // some code goes here
+        int result = Objects.hash(this.tupleno);
+        result = 31 * result;
+        return result;
+        //throw new UnsupportedOperationException("implement this");
+
+    }
+
+}
---
+++ Tuple.java
@@ -0,0 +1,137 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+/**
+ * Tuple maintains information about the contents of a tuple. Tuples have a
+ * specified schema specified by a TupleDesc object and contain Field objects
+ * with the data for each field.
+ */
+public class Tuple implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Field Fields[];
+    private TupleDesc td;
+    private RecordId tdid;
+
+    /**
+     * Create a new tuple with the specified schema (type).
+     *
+     * @param td
+     *            the schema of this tuple. It must be a valid TupleDesc
+     *            instance with at least one field.
+     */
+    public Tuple(TupleDesc td) {
+        // some code goes here
+        this.td = td;
+        this.tdid = null;
+        this.Fields = new Field[td.numFields()];
+        for(int i = 0; i<td.numFields(); i++){
+            Fields[i] = null;
+        }
+    }
+
+    /**
+     * @return The TupleDesc representing the schema of this tuple.
+     */
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+        return this.td;
+    }
+
+    /**
+     * @return The RecordId representing the location of this tuple on disk. May
+     *         be null.
+     */
+    public RecordId getRecordId() {
+        // some code goes here
+        return this.tdid;
+    }
+
+    /**
+     * Set the RecordId information for this tuple.
+     *
+     * @param rid
+     *            the new RecordId for this tuple.
+     */
+    public void setRecordId(RecordId rid) {
+        // some code goes here
+        this.tdid = rid;
+    }
+
+    /**
+     * Change the value of the ith field of this tuple.
+     *
+     * @param i
+     *            index of the field to change. It must be a valid index.
+     * @param f
+     *            new value for the field.
+     */
+    public void setField(int i, Field f) {
+        // some code goes here
+        this.Fields[i] = f;
+    }
+
+    /**
+     * @return the value of the ith field, or null if it has not been set.
+     *
+     * @param i
+     *            field index to return. Must be a valid index.
+     */
+    public Field getField(int i) {
+        // some code goes here
+        return this.Fields[i];
+    }
+
+    /**
+     * Returns the contents of this Tuple as a string. Note that to pass the
+     * system tests, the format needs to be as follows:
+     *
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     *
+     * where \t is any whitespace (except a newline)
+     */
+    public String toString() {
+        // some code goes here
+        throw new UnsupportedOperationException("Implement this");
+    }
+
+    /**
+     * @return
+     *        An iterator which iterates over all the fields of this tuple
+     * */
+    public Iterator<Field> fields()
+    {
+        // some code goes here
+        Iterator itr = new Tuple.ArrayIterator();
+        return itr;
+    }
+
+    /**
+     * reset the TupleDesc of thi tuple
+     * */
+    public void resetTupleDesc(TupleDesc td)
+    {
+        // some code goes here
+        this.td = null;
+    }
+    private class ArrayIterator implements Iterator {
+        int current;
+        public ArrayIterator (){
+            this.current = 0;
+        }
+        public boolean hasNext(){
+            return (current < Fields.length);
+        }
+        public Object next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            current++;
+            return Fields[current - 1];
+        }
+    }
+}
